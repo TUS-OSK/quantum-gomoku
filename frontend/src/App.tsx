c776279890577602932cd1d03bb49bb5a9c5203a
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-01 15:26:13 +0900

    style: set center :art:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Game from './components/Game'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Game from './components/Game';
 
 function App() {
   return (
     <>
       <div className="h-[100dvh] flex flex-col">
         <Header />
-        <div>
-          <h1 className="text-4xl font-bold text-center">Welcome to Gomoku</h1>
-          <Game />
+        <div className="flex justify-center items-center flex-grow">
+          <div className="w-1/2 md:w-1/3">
+            <h1 className="text-4xl font-bold text-center">Welcome to Gomoku</h1>
+            <Game />
+          </div>
         </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
