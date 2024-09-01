0b2f2df6b77aeaf3264fdc764bc1363e926bac94
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-01 15:44:17 +0900

    style: Set className :art:

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -6,7 +6,7 @@ export default function Board() {
   return (
     <>
       {Array(BoardSize).fill(0).map((_, row) => (
-        <div className='board-row' key={row} style={{ display: 'flex' }}>
+        <div className="clear-both table" key={row}>
           {Array(BoardSize).fill(0).map((_, col) => (
             <Stone key={row * BoardSize + col} />
           ))}
--- frontend/src/components/Stone.tsx
+++ frontend/src/components/Stone.tsx
@@ -1,7 +1,7 @@
-export default function Squre() {
+export default function Stone() {
   return (
     <button
-      className="square"
+      className="float-left text-2xl font-bold leading-8 h-8 -mr-px -mt-px p-0 text-center w-8"
     >X
     </button>
   );
