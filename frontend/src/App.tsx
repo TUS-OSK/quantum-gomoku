bdc4fad9b2ea48f654ad752709c076b96750b441
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-01 14:41:35 +0900

    feat: Add Header and Footer base code :sparkles:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="h-[100dvh] flex flex-col">
+        <Header />
+        <div>
+          <h1 className="text-4xl font-bold text-center">Welcome to Gomoku</h1>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
