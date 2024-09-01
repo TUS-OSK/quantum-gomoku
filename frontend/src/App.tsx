c3ff0b8724ea044dc0885b7f247aac7b6ad0ebed
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-01 15:07:38 +0900

    feat: Add Game component :sparkles:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Game from './components/Game'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Header />
         <div>
           <h1 className="text-4xl font-bold text-center">Welcome to Gomoku</h1>
+          <Game />
         </div>
       </div>
       <Footer />
