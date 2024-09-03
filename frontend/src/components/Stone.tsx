bb9e4cb6f190ca54ca78ad480b5489b9d47eebd7
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-03 16:02:13 +0900

    feat: Add Props for Stone component :sparkles:

--- frontend/src/components/Stone.tsx
+++ frontend/src/components/Stone.tsx
@@ -1,8 +1,14 @@
-export default function Stone() {
+export interface StoneProps {
+  value: string | null;
+  onStoneClick: () => void;
+}
+
+export default function Stone({ value, onStoneClick }: StoneProps) {
   return (
     <button
-      className="float-left text-2xl font-bold leading-8 h-8 -mr-px -mt-px p-0 text-center w-8"
-    >X
+      className="float-left text-2xl font-bold leading-8 h-8 -mr-px -mt-px p-0 text-center w-8 border"
+      onClick={onStoneClick}
+    >{value}
     </button>
   );
 }
