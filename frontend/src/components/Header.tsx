bbe9b6631e745b692e21e4cfc5b2666f555148cc
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-03 17:24:35 +0900

    style: fix font color :recycle:

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,10 +2,10 @@ export default function Header() {
   return (
     <header>
       <div className="basis-2/5 flex items-center justify-between">
-        <h1 className="font-bold text-slate-500 tracking-wide text-2xl">
+        <h1 className="font-bold text-black tracking-wide text-2xl">
           Quantum Gomoku
         </h1>
-        <button className="font-bold text-slate-500 hover:text-slate-700">Login</button>
+        <button className="font-bold bg-black text-white hover:bg-gray-600">LOGIN</button>
       </div>
     </header>
   );
