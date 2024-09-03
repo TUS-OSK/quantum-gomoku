98207f611f9df48386446359e5429036992d7e12
n00b <59286911+1m-N00b@users.noreply.github.com>
2024-09-03 16:46:49 +0900

    feat: Game component :tada:

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import Board from './Board';
 
 export default function Game() {
+  const boardSize = 19;
+  const [history, setHistory] = useState([Array(boardSize * boardSize).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentStones = history[currentMove];
+
+  function handlePlay(nextStones: Array<string | null>) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextStones];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  // 履歴ジャンプ機能だが、現状は不要なのでコメントアウト
+  // TODO: Tailwind CSS でスタイリングでのレスポンシブ対応必要
+  // function jumpTo(nextMove: number) {
+  //   setCurrentMove(nextMove);
+  // }
+
+  // const moves = history.map((stones, move) => {
+  //   let description;
+  //   if (move > 0) {
+  //     description = 'Go to move #' + move;
+  //   } else {
+  //     description = 'Go to game start';
+  //   }
+  //   return (
+  //     <li key={move}>
+  //       <button onClick={() => jumpTo(move)}>{description}</button>
+  //     </li>
+  //   );
+  // });
+
   return (
     <div className="flex flex-row">
       <div className="ml-5">
-        <Board />
+        <Board
+          xIsNext={xIsNext}
+          stones={currentStones}
+          onPlay={handlePlay}
+        />
       </div>
     </div>
   );
