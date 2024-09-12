ea6a53c8ae8136eb9f61576a0c471a38c44a779c
oginoshikibu <oginoshikibu@gmail.com>
2024-09-12 13:56:05 +0900

    test utilを用いるよう変更

--- backend/controllers/login_controller_test.go
+++ backend/controllers/login_controller_test.go
@@ -1,102 +1,64 @@
 package controllers
 
 import (
-	"encoding/json"
-	"github.com/gin-gonic/gin"
 	"net/http"
-	"net/http/httptest"
-	"strings"
 	"testing"
+	"github.com/gin-gonic/gin"
+	"gomoku/utils/testutils"
 )
-
-type testRequestBody struct {
-	Message any `json:"message"`
-}
-
-type preferResponse struct {
-	code int
-	body map[string]interface{}
-}
 
 func TestLoginController_Login(t *testing.T) {
 
-	tests := []struct {
-		name    string
-		request testRequestBody
-		want    preferResponse
-	}{
+	tests := []testutils.TestCase{
 		{
-			name: "Test case 1: Success",
-			request: testRequestBody{
-				Message: "Hello",
+			Name: "Test case 1: Success",
+			Request: testutils.Request{
+				Method: http.MethodPost,
+				Url:    "api/v1/users/login",
+				Body: map[string]interface{}{
+					"message": "Hello",
+				},
 			},
-			want: preferResponse{
-				code: http.StatusOK,
-				body: map[string]interface{}{
+			Response: testutils.Response{
+				Code: http.StatusOK,
+				Body: map[string]interface{}{
 					"message": "Login successful",
 				},
 			},
 		},
 		{
-			name:    "Test case 2: Failed with empty",
-			request: testRequestBody{},
-			want: preferResponse{
-				code: http.StatusBadRequest,
-				body: map[string]interface{}{
+			Name: "Test case 2: Failed with empty",
+			Request: testutils.Request{
+				Method: http.MethodPost,
+				Url:    "api/v1/users/login",
+				Body:   map[string]interface{}{},
+			},
+			Response: testutils.Response{
+				Code: http.StatusBadRequest,
+				Body: map[string]interface{}{
 					"error": "Key: 'LoginRequest.Message' Error:Field validation for 'Message' failed on the 'required' tag",
 				},
 			},
 		},
 		{
-			name: "Test case 3: Failed with number",
-			request: testRequestBody{
-				Message: 123,
+			Name: "Test case 3: Failed with number",
+			Request: testutils.Request{
+				Method: http.MethodPost,
+				Url:    "api/v1/users/login",
+				Body: map[string]interface{}{
+					"message": 123,
+				},
 			},
-			want: preferResponse{
-				code: http.StatusBadRequest,
-				body: map[string]interface{}{
+			Response: testutils.Response{
+				Code: http.StatusBadRequest,
+				Body: map[string]interface{}{
 					"error": "json: cannot unmarshal number into Go struct field LoginRequest.message of type string",
 				},
 			},
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			response := httptest.NewRecorder()
-			c, _ := gin.CreateTestContext(response)
-
-			requestBody, _ := json.Marshal(tt.request)
-			c.Request, _ = http.NewRequest(
-				http.MethodPost,
-				"api/v1/users/login",
-				strings.NewReader(string(requestBody)),
-			)
-
-			c.Request.Header.Set("Content-Type", "application/json")
-
-			NewLoginController().Login(c)
-
-			if response.Code != tt.want.code {
-				t.Errorf("Login() got = %v, want %v", response.Code, tt.want.code)
-			} else {
-				var body map[string]interface{}
-				var err = json.Unmarshal(response.Body.Bytes(), &body)
-				if err != nil {
-					t.Errorf("Login() got = %v, want %v", response.Body, tt.want.body)
-				}
-
-				// to compare
-				for key := range tt.want.body {
-					if _, exist := body[key]; exist {
-						if body[key] != tt.want.body[key] {
-							t.Errorf("Login() got = %v, want %v", response.Body, tt.want.body)
-						}
-					} else {
-						t.Errorf("Login() got = %v, want %v", response.Body, tt.want.body)
-					}
-				}
-			}
-		})
-	}
+	testutils.RunControllerTest(t, tests, func() gin.HandlerFunc {
+		return NewLoginController().Login
+	})
 }
--- backend/utils/testutils/controller_test_util.go
+++ backend/utils/testutils/controller_test_util.go
@@ -1,64 +1,68 @@
-package testutil
+package testutils
 
 import (
-    "encoding/json"
-    "net/http"
-    "net/http/httptest"
-    "strings"
-    "testing"
-    "github.com/gin-gonic/gin"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
+type Request struct {
+	Method string
+	Url    string
+	Body   map[string]interface{}
+}
+
+type Response struct {
+	Code int
+	Body map[string]interface{}
+}
+
 type TestCase struct {
-    Name    string
-    Request  struct {
-		Method string
-		Url	string
-		Body map[string]interface{}
-	}
-    Response    struct {
-        Code int
-        Body map[string]interface{}
-    }
+	Name    string
+	Request Request
+	Response Response
 }
 
 func RunControllerTest(t *testing.T, tests []TestCase, createController func() gin.HandlerFunc) {
-    for _, tt := range tests {
-        t.Run(tt.Name, func(t *testing.T) {
-            response := httptest.NewRecorder()
-            c, _ := gin.CreateTestContext(response)
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			response := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(response)
 
-            requestBody, _ := json.Marshal(tt.Request.Body)
-            c.Request, _ = http.NewRequest(
+			requestBody, _ := json.Marshal(tt.Request.Body)
+			c.Request, _ = http.NewRequest(
 				tt.Request.Method,
 				tt.Request.Url,
 				strings.NewReader(string(requestBody)),
-            )
+			)
 
-            c.Request.Header.Set("Content-Type", "application/json")
+			c.Request.Header.Set("Content-Type", "application/json")
 
-            createController()(c)
+			createController()(c)
 
-            if response.Code != tt.Response.Code {
-                t.Errorf("got = %v, want %v", response.Code, tt.Response.Code)
-            } else {
-                var body map[string]interface{}
-                var err = json.Unmarshal(response.Body.Bytes(), &body)
-                if err != nil {
-                    t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
-                }
+			if response.Code != tt.Response.Code {
+				t.Errorf("got = %v, want %v", response.Code, tt.Response.Code)
+			} else {
+				var body map[string]interface{}
+				var err = json.Unmarshal(response.Body.Bytes(), &body)
+				if err != nil {
+					t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
+				}
 
-                // to compare
-                for key := range tt.Response.Body {
-                    if _, exist := body[key]; exist {
-                        if body[key] != tt.Response.Body[key] {
-                            t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
-                        }
-                    } else {
-                        t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
-                    }
-                }
-            }
-        })
-    }
+				// to compare
+				for key := range tt.Response.Body {
+					if _, exist := body[key]; exist {
+						if body[key] != tt.Response.Body[key] {
+							t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
+						}
+					} else {
+						t.Errorf("got = %v, want %v", response.Body, tt.Response.Body)
+					}
+				}
+			}
+		})
+	}
 }
