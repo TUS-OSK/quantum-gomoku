b9b3649a3504ed44850da12429ea5e3751e7a142
oginoshikibu <oginoshikibu@gmail.com>
2024-09-20 11:40:54 +0900

    mocの実装

--- backend/controllers/search_users_controller.go
+++ backend/controllers/search_users_controller.go
@@ -15,8 +15,40 @@ func NewSearchUsersController() *SearchUsersController {
 	return &SearchUsersController{}
 }
 
+type User struct {
+	Username string `json:"username"`
+	Icon     string `json:"icon"`
+}
+
+type SearchUsersResponse struct {
+	Users []User `json:"users"`
+}
 
 // SearchUsers is a function to search users
 func (controller *SearchUsersController) SearchUsers(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"users": "users"})
+	user_suffix := context.Query("username")
+	if user_suffix == "" {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "query parameter 'username' is required"})
+		return
+	}
+	response := SearchUsersResponse{}
+
+	switch user_suffix {
+	case "OSK":
+		response.Users = append(response.Users, User{Username: "OSK", Icon: "https://oskt.us/favicon.ico"})
+	case "OSS":
+		response.Users = append(response.Users, User{Username: "OSS"})
+	case "OS":
+		response.Users = append(response.Users,
+			User{Username: "OSK", Icon: "https://oskt.us/favicon.ico"},
+			User{Username: "OSS"},
+		)
+	case "O":
+		response.Users = append(response.Users,
+			User{Username: "O", Icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Latin_letter_O.svg/1920px-Latin_letter_O.svg.png"},
+			User{Username: "OSK", Icon: "https://oskt.us/favicon.ico"},
+			User{Username: "OSS"})
+	}
+
+	context.JSON(http.StatusOK, response)
 }
