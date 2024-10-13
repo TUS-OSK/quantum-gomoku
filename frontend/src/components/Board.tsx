e780371ca71cde8ad72a888c98ba5fa214450223
oginoshikibu <oginoshikibu@gmail.com>
2024-10-13 23:22:33 +0900

    cellSizeを変数で切り出し

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -8,6 +8,8 @@ interface BoardProps {
 
 export default function Board({ xIsNext, changeTurn }: BoardProps) {
   const boardSize = 19;
+  const minCellSize = 20;
+  const maxCellSize = 40;
 
   const Stones = Array(boardSize * boardSize).fill(null).map((_, index) => (
     <Stone key={index} changeTurn={changeTurn} xIsNext={xIsNext} />
@@ -27,24 +29,24 @@ export default function Board({ xIsNext, changeTurn }: BoardProps) {
       <div className="mb-2 flex justify-center items-center">{status}</div>
       <div
         className="flex justify-center items-center"
-        style={{ minWidth: `${boardSize * 20 + (boardSize - 1) * 4}px` }}
+        style={{ minWidth: `${boardSize * minCellSize + (boardSize - 1) * 4}px` }}
       >
         <div
           className="grid border-2 border-black"
           style={{
-            gridTemplateColumns: `repeat(${boardSize}, minmax(20px, 40px))`,
-            gridTemplateRows: `repeat(${boardSize}, minmax(20px, 40px))`,
+            gridTemplateColumns: `repeat(${boardSize}, minmax(${minCellSize}px, ${maxCellSize}px))`,
+            gridTemplateRows: `repeat(${boardSize}, minmax(${minCellSize}px, ${maxCellSize}px))`,
             width: '100%',
             aspectRatio: '1',
           }}
         >
           {
             Stones.map((Stone, index) => {
-              const isRightBox = (index + 1) % boardSize === 0;
-              const isBottomBox = index >= boardSize * (boardSize - 1);
+              const isRightCell = (index + 1) % boardSize === 0;
+              const isBottomCell = index >= boardSize * (boardSize - 1);
               return (
                 <div className={
-                  `border-black ${isRightBox ? '' : ' border-r'} ${isBottomBox ? '' : ' border-b'}`
+                  `border-black ${isRightCell ? '' : ' border-r'} ${isBottomCell ? '' : ' border-b'}`
                 }>
                   {Stone}
                 </div>
