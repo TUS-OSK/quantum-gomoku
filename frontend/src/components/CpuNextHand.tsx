8199518e906101c456e1d9d339331ce127b4e007
oginoshikibu <oginoshikibu@gmail.com>
2024-11-23 02:26:19 +0900

    相手の石も評価するように変更

--- frontend/src/components/CpuNextHand.tsx
+++ frontend/src/components/CpuNextHand.tsx
@@ -4,52 +4,54 @@ type CpuProps = {
     nextStoneProbability: number;
 }
 
+
 const CpuNextHand = ({ stoneProbabilities, boardSize, nextStoneProbability }: CpuProps): number => {
 
     const stoneProbabilitiesGrid = Array.from({ length: boardSize }, (_, i) =>
         stoneProbabilities.slice(i * boardSize, (i + 1) * boardSize)
     );
 
-    const neighbors = new Set<string>();
+    // 隣接している空白マスの集合を取得
+    const neighbors = new Set<{ i: number, j: number }>();
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
             if (stoneProbabilitiesGrid[i][j] !== null) {
-                for (const [di, dj] of [
-                    [0, 1],
-                    [1, 0],
-                    [0, -1],
-                    [-1, 0],
-                ]) {
+                for (const [di, dj] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
                     const ni = i + di;
                     const nj = j + dj;
                     if (
-                        0 <= ni &&
-                        ni < boardSize &&
-                        0 <= nj &&
-                        nj < boardSize &&
+                        0 <= ni && ni < boardSize &&
+                        0 <= nj && nj < boardSize &&
                         stoneProbabilitiesGrid[ni][nj] === null
                     ) {
-                        neighbors.add(`${ni},${nj}`);
+                        neighbors.add({ i: ni, j: nj });
                     }
                 }
             }
         }
     }
 
-    console.log(`neighbors=${JSON.stringify(Array.from(neighbors))}`);
+    const maxEvaluationValues = Array(6).fill(-1);
+    const maxPositions = Array(6).fill(-1);
+    const nextNextStoneProbability = nextStoneProbability === 10 ? 70 : 90;
 
-    const max_val = Array(6).fill(-1);
-    const res_num = Array(6).fill(-1);
+    const evaluattonValuesGrid = Array.from({ length: boardSize }, () =>
+        Array.from({ length: boardSize }, () => -1)
+    );
 
     for (const neighbor of neighbors) {
-        const [i, j] = neighbor.split(",").map(Number);
+        const i = neighbor.i;
+        const j = neighbor.j;
+        let maxEvaluationValue = -1;
         for (const [di, dj] of [[0, 1], [1, 0], [1, 1], [1, -1],]) {
             const line: number[] = [];
+            let centerIdx = -1;
             for (let num = -4; num <= 4; num++) {
                 const ni = i + di * num;
                 const nj = j + dj * num;
                 if (num === 0) {
                     line.push(nextStoneProbability);
+                    centerIdx = line.length - 1;
                     continue;
                 }
                 if (0 <= ni && ni < boardSize && 0 <= nj && nj < boardSize) {
@@ -66,37 +68,49 @@ const CpuNextHand = ({ stoneProbabilities, boardSize, nextStoneProbability }: Cp
             }
             if (line.length >= 5) {
                 const D: number[] = [];
-                let cur = 1;
+                let cpuWinEvaluateValue = 1;
+                let playerWinEvaluateValue = 1;
                 for (let idx = 0; idx < line.length; idx++) {
                     const l = line[idx];
-                    D.push(100 - l);
-                    cur *= 100 - l;
+                    D.push(l);
+                    cpuWinEvaluateValue *= 100 - l;
+                    playerWinEvaluateValue *= centerIdx === idx ? nextNextStoneProbability : l;
                     if (idx < 5) continue;
-                    if (max_val[5] < cur) {
-                        max_val[5] = cur;
-                        res_num[5] = i * boardSize + j;
+                    if (maxEvaluationValue < cpuWinEvaluateValue + playerWinEvaluateValue) {
+                        maxEvaluationValue = cpuWinEvaluateValue + playerWinEvaluateValue;
                     }
-                    cur /= D.shift()!;
+                    if (maxEvaluationValues[5] < cpuWinEvaluateValue + playerWinEvaluateValue) {
+                        maxEvaluationValues[5] = cpuWinEvaluateValue + playerWinEvaluateValue;
+                        maxPositions[5] = i * boardSize + j;
+                    }
+                    cpuWinEvaluateValue /= 100 - D.shift()!;
+                    playerWinEvaluateValue /= D.shift()!;
                 }
             } else {
-                let cur = 1;
+                let cpuWinEvaluateValue = 1;
+                let playerWinEvaluateValue = 1;
                 for (const l of line) {
-                    cur *= 100 - l;
+                    cpuWinEvaluateValue *= 100 - l;
+                    playerWinEvaluateValue *= centerIdx === 0 ? nextNextStoneProbability : l;
                 }
-                if (max_val[line.length] < cur) {
-                    max_val[line.length] = cur;
-                    res_num[line.length] = i * boardSize + j;
+                if (maxEvaluationValue < cpuWinEvaluateValue + playerWinEvaluateValue) {
+                    maxEvaluationValue = cpuWinEvaluateValue + playerWinEvaluateValue;
                 }
+                if (maxEvaluationValues[line.length] < cpuWinEvaluateValue + playerWinEvaluateValue) {
+                    maxEvaluationValues[line.length] = cpuWinEvaluateValue + playerWinEvaluateValue;
+                    maxPositions[line.length] = i * boardSize + j;
+                }
+
             }
         }
+        evaluattonValuesGrid[i][j] = maxEvaluationValue;
     }
 
-    console.log(`max_val=${JSON.stringify(max_val)}`);
-    console.log(`res_num=${JSON.stringify(res_num)}`);
+    console.log(evaluattonValuesGrid);
 
     for (let i = 5; i >= 0; i--) {
-        if (res_num[i] !== -1) {
-            return res_num[i];
+        if (maxPositions[i] !== -1) {
+            return maxPositions[i];
         }
     }
     return -1;
